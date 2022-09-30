58a011893477011636e34264dc7812b9340cf85d
insomnia-ins <nikita.ins@mail.ru>
2022-09-30 10:00:13 +0900

    testing sqlite

--- 1.py
+++ 1.py
@@ -1,1 +1,25 @@
-print("its inso")
+import sqlite3
+
+connect = sqlite3.connect('people.db')
+cursor = connect.cursor()
+
+def query_users():
+    cursor.execute("SELECT * FROM users;")
+    all_results = cursor.fetchall()
+
+    request_query = []
+    request_query.append(int(all_results[-1][0])+1)
+    request_query.append(input("Name: "))
+    request_query.append(input("Age: "))
+    request_query.append(input("Gender: "))
+    
+    request_query_ready = tuple(request_query)
+    
+    cursor.execute("INSERT INTO users VALUES(?, ?, ?, ?);", request_query_ready)
+    connect.commit()
+    
+
+    
+    
+
+query_users()
